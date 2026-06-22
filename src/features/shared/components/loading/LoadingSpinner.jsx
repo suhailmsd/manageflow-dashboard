9e358b413c5d0c1ae325303599f801eaf055b61d
suhailmsd <mohammedsuhaild666@gmail.com>
2026-06-22 10:23:46 +0300

    small bug fix on header UI render

--- src/features/shared/components/loading/LoadingSpinner.jsx
+++ src/features/shared/components/loading/LoadingSpinner.jsx
@@ -1,9 +1,11 @@
 import { FaSpinner } from "react-icons/fa";
+import useModal from "../../hooks/useModal";
 
 export default function LoadingSpinner() {
+  const {isModalOpen} = useModal();
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <FaSpinner className="animate-spin text-3xl rounded-full text-bold text-black dark:text-white" />
+      <FaSpinner className={`animate-spin text-3xl rounded-full text-bold text-black dark:text-white ${isModalOpen && 'text-white'}`} />
     </div>
   );
 }
